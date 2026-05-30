188d1baff8b37fb079d856ec4adcd7bdc4949103
yindaheng98 <yindaheng98@gmail.com>
2026-05-29 20:50:25 -0700

    strict

--- src/runtime/config.ts
+++ src/runtime/config.ts
@@ -5,15 +5,22 @@ export type RuntimeDefinition<K extends RuntimeKind = RuntimeKind> = {
   [P in K]: { kind: P; options?: RuntimeOptions<P> };
 }[K];
 
-export type ThreadDefinition = {
+export type ThreadDefinitionForKind<K extends RuntimeKind> = {
   runtime: string;
-  options?: ThreadOptions;
+  options?: ThreadOptions<K>;
 };
+
+export type ThreadDefinition<K extends RuntimeKind = RuntimeKind> = {
+  [P in K]: ThreadDefinitionForKind<P>;
+}[K];
 
 export function createRuntime<K extends RuntimeKind>(runtime: RuntimeDefinition<K>): Runtime<K> {
   return getRuntimeFactory(runtime.kind)(runtime.options);
 }
 
-export function startThread(runtime: Runtime, thread: ThreadDefinition): Promise<Thread> {
-  return runtime.startThread(thread.options ?? {});
+export function startThread<K extends RuntimeKind>(
+  runtime: Runtime<K>,
+  options?: ThreadOptions<K>,
+): Promise<Thread<K>> {
+  return runtime.startThread(options);
 }
