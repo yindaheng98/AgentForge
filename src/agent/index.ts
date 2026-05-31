ee3e653f3efc738f84d0dcec1ee5cda3b373cd4d
yindaheng98 <yindaheng98@gmail.com>
2026-05-30 23:06:26 -0700

    export

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -1,5 +1,11 @@
 export { Agent } from "./agent.js";
 export type { PromptVariables, PromptConstants } from "./agent.js";
+export {
+  PromptTemplateAgent,
+  formatPromptTemplate,
+  mergePromptTemplateVariables,
+} from "./template.js";
+export type { PromptTemplateConstants } from "./template.js";
 export { AgentTeam } from "./team.js";
 export type { AgentFactory, AgentFactoryMap, AgentVariablesByName } from "./team.js";
 export type { AgentDefinition } from "./config.js";
