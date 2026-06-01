ecd0d6ac1ce88187b00ad81ec354d5a82e889174
yindaheng98 <yindaheng98@gmail.com>
2026-06-01 10:40:20 -0700

    less export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export * from "./config.js";
-export * from "./agent/index.js";
-export * from "./runtime/index.js";
+export { defineConfig, loadConfig } from "./config.js";
+export {
+  Agent,
+  AgentTeam,
+  PromptTemplateAgent,
+  formatPromptTemplate,
+} from "./agent/index.js";
+export { createRuntime, startThread } from "./runtime/index.js";
+
+export type { Config } from "./config.js";
+
+export type {
+  AgentDefinition,
+  AgentFactory,
+  AgentFactoryMap,
+  AgentVariablesByName,
+  PromptConstants,
+  PromptVariables,
+  PromptTemplateConstants,
+} from "./agent/index.js";
+
+export type {
+  Runtime,
+  RuntimeKind,
+  RuntimeOptions,
+  RuntimeRecord,
+  Thread,
+  ThreadOptions,
+  RecordCallback,
+} from "./runtime/index.js";
