5c5da20c30607104ef172157be7eef18d1e49a65
yindaheng98 <yindaheng98@gmail.com>
2026-05-31 23:47:08 -0700

    just export all

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-export { defineConfig, loadConfig } from "./config.js";
-export { Agent, AgentTeam } from "./agent/index.js";
-export type { Config } from "./config.js";
-export type { AgentDefinition } from "./agent/config.js";
-export type { AgentFactory, AgentFactoryMap, AgentVariablesByName } from "./agent/team.js";
-export type { PromptConstants, PromptVariables } from "./agent/agent.js";
-export type {
-  Runtime,
-  RuntimeKind,
-  RuntimeOptions,
-  RuntimeRecord,
-  Thread,
-  ThreadOptions,
-  RecordCallback,
-} from "./runtime/types.js";
+export * from "./config.js";
+export * from "./agent/index.js";
+export * from "./runtime/index.js";
