9dbb70ffa55936bf18486525c216f044222150d9
yindaheng98 <yindaheng98@gmail.com>
2026-06-10 20:56:38 -0700

    export

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,21 @@ export { defineConfig, loadConfig, mergeConfig } from "./config.js";
 export { Agent, AgentTeam, PromptTemplateAgent, formatPromptTemplate } from "./agent/index.js";
 export { createRuntime, startThread } from "./runtime/index.js";
 export { isPlainObject, loadYamls, mergePlainObjects, type PlainObject } from "./utils/index.js";
+export {
+  definePipeline,
+  parsePipelineArgs,
+  runPipelineCli,
+  runPipelinesCli,
+} from "./pipeline/index.js";
 
 export type { Config } from "./config.js";
+
+export type {
+  Pipeline,
+  PipelineArgsOption,
+  PipelineArgsOptions,
+  PipelineOptions,
+} from "./pipeline/index.js";
 
 export type {
   AgentDefinition,
