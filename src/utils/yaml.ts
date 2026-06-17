8de6a6b18ddd00ee0ec6df23075fbf2d99bd8eeb
yindaheng98 <yindaheng98@gmail.com>
2026-06-17 15:09:47 -0700

    expandYamlAliases

--- src/utils/yaml.ts
+++ src/utils/yaml.ts
@@ -2,6 +2,18 @@ import { readFile } from "node:fs/promises";
 import { parse } from "yaml";
 import { isPlainObject, mergePlainObjects, type PlainObject } from "./object.js";
 
+function expandYamlAliases(value: unknown): unknown {
+  if (Array.isArray(value)) {
+    return value.map(expandYamlAliases);
+  }
+  if (isPlainObject(value)) {
+    return Object.fromEntries(
+      Object.entries(value).map(([key, entry]) => [key, expandYamlAliases(entry)]),
+    );
+  }
+  return value;
+}
+
 export async function loadYamls(...paths: string[]): Promise<PlainObject> {
   if (paths.length === 0) {
     throw new Error("loadYamls requires at least one path");
@@ -9,7 +21,7 @@ export async function loadYamls(...paths: string[]): Promise<PlainObject> {
 
   let object: PlainObject = {};
   for (const path of paths) {
-    const nextObject = parse(await readFile(path, "utf8")) as unknown;
+    const nextObject = expandYamlAliases(parse(await readFile(path, "utf8")));
     if (!isPlainObject(nextObject)) {
       throw new Error(`YAML file must contain an object: ${path}`);
     }
